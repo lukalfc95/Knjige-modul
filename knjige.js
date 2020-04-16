2e010a178d334970b782809867ad94473ed4a8ae
lukalfc95 <62513254+lukalfc95@users.noreply.github.com>
2020-04-16 20:43:31 +0200

    Add files via upload

---
+++ knjige.js
@@ -0,0 +1,27 @@
+exports.knjige = [
+    {
+        "id":1,
+        "naziv":"Ana Karenjina",
+        "autor":"Lav N. Tolstoj"
+    },
+    {
+        "id":2,
+        "naziv":"Hobit",
+        "autor":"J. R. R. Tolkin"
+    },
+    {
+        "id":3,
+        "naziv":"Igre Gladi",
+        "autor":"Suzan Kolins"
+    },
+    {
+        "id":4,
+        "naziv":"Tvrdjava",
+        "autor":"Mesa Selimovic"
+    },
+    {
+        "id":5,
+        "naziv":"Beogradske price",
+        "autor":"Ivo Andric"
+    }
+];
